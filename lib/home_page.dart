29efa16ad0aadebe9f8c4378aadaab7e143a0d9e
Bhushan Pagare <bpagare6@gmail.com>
2026-03-23 18:44:25 +0530

    some pending changes

---
+++ lib/home_page.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'form_page.dart';
+import 'global_state.dart';
+import 'seats_page.dart';
+import 'students_page.dart';
+import 'supabase_service.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final supbaseService = SupabaseService();
+  int _selectedIndex = 0;
+  List<String> _allowedBranches = [];
+  String? _selectedBranch;
+  Map<String, Map<String, dynamic>> _branchDetailsMap = {};
+  List<Map<String, dynamic>> _students = [];
+  late Future<void> _initializationFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializationFuture = _authenticateAndLoadBranches();
+  }
+
+  Future<void> _authenticateAndLoadBranches() async {
+    await _loadBranches();
+    await _loadStudents();
+    _updateGlobalState();
+  }
+
+  Future<void> _loadBranches() async {
+    final branchDetailsMap = await supbaseService.getAllBranchDetails();
+
+    setState(() {
+      _allowedBranches = branchDetailsMap.keys.toList();
+      _branchDetailsMap = branchDetailsMap;
+      if (_allowedBranches.isNotEmpty) {
+        _selectedBranch = _allowedBranches[0];
+      }
+    });
+  }
+
+  Future<void> _loadStudents() async {
+    if (_selectedBranch != null) {
+      final students = await supbaseService.getStudentsByBranchId(
+        _branchDetailsMap[_selectedBranch]!['id'],
+      );
+
+      setState(() {
+        _students = students;
+      });
+    } else {
+      setState(() {
+        _students = [];
+      });
+    }
+  }
+
+  void _updateGlobalState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final globalState = Provider.of<GlobalState>(context, listen: false);
+      final branchDetails =
+      _selectedBranch != null ? _branchDetailsMap[_selectedBranch] : null;
+
+      globalState.setStudents(_students);
+      globalState.setBranchDetails(branchDetails);
+    });
+  }
+
+  void _onFormSubmitted() async {
+    await _loadStudents();
+    _updateGlobalState();
+  }
+
+  void _navigateToFormPage(branchDetails, students) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FormPage()),
+    );
+
+    if (result == true) {
+      _onFormSubmitted();
+    }
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onLogout() {
+    // Handle logout logic here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: _initializationFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(child: Text('Error: ${snapshot.error}')),
+          );
+        } else {
+          final branchDetails =
+          _selectedBranch != null
+              ? _branchDetailsMap[_selectedBranch]
+              : null;
+
+          final List<Widget> widgetOptions = <Widget>[
+            branchDetails == null
+                ? CircularProgressIndicator()
+                : SeatsPage(onFormSubmitted: _onFormSubmitted),
+            branchDetails == null
+                ? CircularProgressIndicator()
+                : StudentsPage(onFormSubmitted: _onFormSubmitted),
+          ];
+
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 240,
+                    child: DropdownButton<String>(
+                      value: _selectedBranch,
+                      items:
+                      _allowedBranches.map<DropdownMenuItem<String>>((
+                          String value,
+                          ) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: TextStyle(fontSize: 24),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) async {
+                        setState(() {
+                          _selectedBranch = newValue!;
+                        });
+                        await _loadStudents();
+                        _updateGlobalState();
+                      },
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.logout),
+                    onPressed: _onLogout,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+            body: Center(
+              child:
+              branchDetails == null
+                  ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 16),
+                  Text('Loading branch details from database'),
+                ],
+              )
+                  : widgetOptions.elementAt(_selectedIndex),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed:
+              branchDetails == null
+                  ? null
+                  : () => _navigateToFormPage(branchDetails, _students),
+              shape: CircleBorder(),
+              backgroundColor: Colors.lightBlueAccent,
+              child: Icon(Icons.add, size: 32),
+            ),
+            floatingActionButtonLocation:
+            FloatingActionButtonLocation.centerDocked,
+            bottomNavigationBar: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.event_seat, size: 28),
+                    label: 'Seats',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person, size: 28),
+                    label: 'Students',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.indigo,
+                backgroundColor: Colors.white,
+                unselectedItemColor: Colors.grey,
+                onTap: _onItemTapped,
+                type: BottomNavigationBarType.fixed,
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => MyHomePage()), // Replace with your main screen
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/dnyanjyoti_logo.png'),
+          SizedBox(height: 10),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
